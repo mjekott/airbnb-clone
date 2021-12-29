c0c22db2ca7f72f2cf7872e6597516a06aa4dad4
mfonobong ekott <ekottmfon@yahoo.com>
2021-12-29 18:33:21 +0100

    feat: implement reservation model

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.Reservation)
+class ResercationAdmin(admin.ModelAdmin):
+    pass
--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from core import models as core_models
 
 # Create your models here.
+
+
+class Reservation(core_models.TimeStamedModel):
+
+    """Reservation Model Definition"""
+
+    STATUS_PENDING = "pending"
+    STATUS_COMFIRMED = "confirmed"
+    STATUS_CANCELED = "canceled"
+
+    STATUS_CHOICES = (
+        (STATUS_PENDING, "Pendings"),
+        (STATUS_CANCELED, "Canceled"),
+        (STATUS_COMFIRMED, "Confirmed"),
+    )
+
+    status = models.CharField(
+        choices=STATUS_CHOICES, default=STATUS_PENDING, max_length=10
+    )
+    check_in = models.DateField()
+    check_out = models.DateField()
+
+    guest = models.ForeignKey("users.User", on_delete=models.CASCADE)
+
+    room = models.ForeignKey("rooms.ROOM", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.room}-{self.check_in}"
