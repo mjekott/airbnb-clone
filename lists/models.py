ff49b4d8b8b5d6430e2c7adf97d53dbf96e19b23
mfonobong ekott <ekottmfon@yahoo.com>
2021-12-29 18:45:36 +0100

    feat: implement list  model

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from core import models as core_models
+from rooms import models as room_models
 
 # Create your models here.
+
+
+class List(core_models.TimeStamedModel):
+    """List Model Definition"""
+
+    name = models.CharField(max_length=80)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    rooms = models.ManyToManyField(room_models.Room, blank=True)
+
+    def __str__(self):
+        return self.name
