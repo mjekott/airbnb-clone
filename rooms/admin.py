5ae6e4943fdba75b36c8d354bf1c48d8f39f11e2
mfonobong ekott <ekottmfon@yahoo.com>
2021-12-29 19:47:24 +0100

    feat: Adding filter and ordering fucntionality to rooms admin panel

--- rooms/admin.py
+++ rooms/admin.py
@@ -8,6 +8,57 @@ from . import models
 class RoomAdmin(admin.ModelAdmin):
     """Room Admin Definition"""
 
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+    )
+
+    ordering = ("name", "price", "bedrooms")
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    list_filter = (
+        "instant_book",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "host__superhost",
+        "city",
+        "country",
+    )
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
+
+    search_fields = ("^city", "^host__username")
+
 
 @admin.register(models.RoomType, models.Amenity, models.HouseRule, models.Facility)
 class ItemAdmin(admin.ModelAdmin):
